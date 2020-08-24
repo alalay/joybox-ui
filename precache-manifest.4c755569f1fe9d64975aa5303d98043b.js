4208bb5402ea43541db8dbd2baee1e4d45988f44
alalay <52942451+alalay@users.noreply.github.com>
2020-08-24 09:42:53 +0000

    Deploying to gh-pages from  @ ebe6be6029265fcac6607f29e1591a321dced48b 🚀

---
+++ precache-manifest.4c755569f1fe9d64975aa5303d98043b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "901e15b33c27b68da99a292b19d97bae",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.27edf03838d22fa89e80.bundle.js"
+  },
+  {
+    "url": "runtime~main.27edf03838d22fa89e80.bundle.js"
+  },
+  {
+    "revision": "6e465cd68e7a9a5746e8",
+    "url": "static/css/main.cc6f50a5.chunk.css"
+  },
+  {
+    "url": "vendors~main.27edf03838d22fa89e80.bundle.js"
+  },
+  {
+    "url": "vendors~main.27edf03838d22fa89e80.bundle.js.LICENSE.txt"
+  }
+]);
